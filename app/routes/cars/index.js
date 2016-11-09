0e45aff78be117e71ed4476c750200ba118261fc
Andrew Henrique <andrewhenriquef@gmail.com>
2016-11-09 11:24:55 -0200

    Second part of bug-fixes of route car.new

---
+++ app/routes/cars/index.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+/*
+var cars = [
+    {
+        "id": 1,
+        "make": "Honda",
+        "model": "Accord",
+        "year": "2007"
+
+    },
+    {
+        "id": 2,
+        "make": "Toyota",
+        "model": "Camry",
+        "year": "2013"
+
+    },
+    {
+        "id": 3,
+        "make": "Ford",
+        "model": "Explorer",
+        "year": "2002"
+
+    }
+]
+*/
+export default Ember.Route.extend({
+    model: function(){
+        return this.store.findAll('car');
+
+        /*return Ember.$.getJSON('cars.json').then(function(data){
+            return data; 
+        });*/
+    }
+});
