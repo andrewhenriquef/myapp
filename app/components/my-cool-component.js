fe129323be8cf6d89ade543bbe90caad1089d6d6
Andrew Henrique <andrewhenriquef@gmail.com>
2016-11-13 19:56:31 -0200

    Setting changes in component to show some information and change background color, hide the button when greetingSet value is true

--- app/components/my-cool-component.js
+++ app/components/my-cool-component.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    name: 'Andrew',
+    greetingSet: false,
+
+    actions: {
+        greetUser: function(name){
+
+            alert('Hello ' + name);
+            this.setProperties({
+                greetingSet: true
+
+            });
+        }
+
+    },
+    classNames: ['well'],
+    classNameBindings: ['greetingSet'],
+
 });
